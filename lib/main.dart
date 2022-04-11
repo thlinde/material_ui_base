f89a7106027ed4632ebe16f6bab1442faa8b44c3
thlinde <thlinde@me.com>
2022-04-11 15:41:27 +0200

    introduce getx and get_storage on exit store size/position start at last size/position

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:easy_sidemenu/easy_sidemenu.dart';
+
+import 'model/store.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
+  await GetStorage.init();
+  final ProgController prog = Get.put(ProgController());
 
-  // Use it only after calling `hiddenWindowAtLaunch`
   windowManager.waitUntilReadyToShow().then((_) async{
-    // Hide window title bar
     await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
-    await windowManager.setSize(const Size(800, 600));
-    await windowManager.center();
+    await windowManager.setSize(Size(prog.width, prog.height));
+    await windowManager.setPosition(Offset(prog.xPos, prog.yPos));
+    await windowManager.setPreventClose(true);
     await windowManager.show();
     await windowManager.setSkipTaskbar(false);
   });
@@ -30,134 +36,12 @@ class ThlindeApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(title: 'thlinde App'),
     );
   }
 }
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({Key? key, required this.title}) : super(key: key);
-//
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-//
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-//
-//   final String title;
-//
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-//
-// class _HomePageState extends State<HomePage> with WindowListener {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-//
-//   @override
-//   void initState() {
-//     windowManager.addListener(this);
-//     super.initState();
-//   }
-//
-//   @override
-//   void dispose() {
-//     windowManager.removeListener(this);
-//     super.dispose();
-//   }
-//
-//   @override
-//   void onWindowFocus() {
-//     // Make sure to call once.
-//     setState(() {});
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       // appBar: AppBar(
-//       //   // Here we take the value from the HomePage object that was created by
-//       //   // the App.build method, and use it to set our appbar title.
-//       //   title: Text(widget.title),
-//       // ),
-//       appBar: PreferredSize(
-//         child: WindowCaption(
-//           brightness: Theme.of(context).brightness,
-//           title: const Text('window_manager_example'),
-//           backgroundColor: Colors.grey[300],
-//         ),
-//         preferredSize: const Size.fromHeight(kWindowCaptionHeight),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
-
-
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -168,8 +52,21 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with WindowListener {
+  final ProgController prog = Get.find();
   PageController page = PageController();
+
+  @override
+  void initState() {
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -211,6 +108,7 @@ class _HomePageState extends State<HomePage> {
                   // child: Image.asset(
                   //   'assets/images/easy_sidemenu.png',
                   // ),
+                  child: const Text('thlinde'),
                 ),
                 const Divider(
                   indent: 8.0,
@@ -333,5 +231,23 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  @override
+  void onWindowClose() async {
+    bool _isPreventClose = await windowManager.isPreventClose();
+    if (_isPreventClose) {
+      await saveProgSettings();
+      windowManager.destroy();
+    }
+  }
+
+  saveProgSettings() async {
+    final pos = await windowManager.getPosition();
+    prog.setXPos(pos.dx);
+    prog.setyPos(pos.dy);
+    final size = await windowManager.getSize();
+    prog.setHeight(size.height);
+    prog.setWidth(size.width);
   }
 }
